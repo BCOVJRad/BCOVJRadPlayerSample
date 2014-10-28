82548305ac4b3d1328d7981b8bc25c9ee313c755
Joe Radjavitch <jradjavitch>
2014-10-28 10:47:31 -0400

    Initial Commit

---
+++ BCOVJRadPlayerSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BCOVJRadPlayerSample
+//
+//  Created by Joseph Radjavitch on 10/28/14.
+//  Copyright (c) 2014 Joseph Radjavitch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
